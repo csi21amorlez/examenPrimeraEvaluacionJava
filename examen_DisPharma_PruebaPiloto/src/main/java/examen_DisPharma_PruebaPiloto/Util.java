d64620ed0d5b3053da2131bce40d4b41b8c8bfe1
DESKTOP-QMGOQCG\CSI21-Amorlez <csi21-amorlez@colegioaltair.net>
2022-10-13 11:04:30 +0200

    Commit inicial -- Version 131022

---
+++ examen_DisPharma_PruebaPiloto/src/main/java/examen_DisPharma_PruebaPiloto/Util.java
@@ -0,0 +1,34 @@
+package examen_DisPharma_PruebaPiloto;
+
+/*
+ * Util --> En esta clase almacenaremos las variables de conexión a la base de datos.
+ */
+public class Util {
+	
+//	Declaramos las constantes y las inicializamos
+	static final String HOST = "localhost";	
+	static final String PASSW = "root123";
+	static final int PORT = 5432;
+	static final String DB = "General";
+	static final String USER = "postgres";
+	
+	
+//	Definimos los metodos por los que accederemos a las constantes
+	public static String getHost() {
+		return HOST;
+	}
+	public static String getPassw() {
+		return PASSW;
+	}
+	public static int getPort() {
+		return PORT;
+	}
+	public static String getDb() {
+		return DB;
+	}
+	public static String getUser() {
+		return USER;
+	}
+	
+
+}
